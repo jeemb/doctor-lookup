03f7895850a0cb77a3775393c3ab7f3b241d5777
Jennifer Beem <jenniferbeem@gmail.com>
2017-03-17 12:51:53 -0700

    change first name to last name display

--- js/Doctor.js
+++ js/Doctor.js
@@ -9,23 +9,12 @@ Doctor.prototype.searchSymptom = function(symptom, callback) {
   $.get('https:api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
   .then(function(response) {
     console.log(response);
-    var doctor_one = response.data[0].profile.first_name;
     callback(symptom, response);
-    // $('.show_doctors').append("<ul>");
-    // for (var i = 0; i < response.profile.length; i++) {
-    //   $('.show_doctors').append("<li>" + response.profile[i].first_name + "</li>");
-    // }
-    // $('.show_doctors').append("</ul>");
+  })
+
+  .fail(function(error) {
+    $('.show_doctors').text(error.responseJSON.message);
   });
-
-  // .fail(function(error) {
-  //   $('.show_doctors').text(error.responseJSON.message);
-  // });
-  // };
-
-  // function display(symptom, doctor) {
-  //   $('.show_doctor').text("Doctor" + doctor);
-  // }
-
 };
+
 exports.doctorModule = Doctor;
--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,11 @@
 var Doctor = require('./../js/Doctor.js').doctorModule;
 
 var displayDoctors = function(symptom, response) {
-  $('.show_doctors').append("<ul>");
+  $('.show_doctors').append("<p> List of Doctors: </p> <ul>");
   for (var i = 0; i < response.data.length; i++) {
-    $('.show_doctors').append("<li>" + response.data[i].profile.first_name + "</li>");
+    $('.show_doctors').append("<li> Dr. " + response.data[i].profile.last_name + "</li>");
   }
     $('.show_doctors').append("</ul>");
-  // "The best doctor for " + symptom + "is" + doctor);
 };
 
 $(document).ready(function() {
